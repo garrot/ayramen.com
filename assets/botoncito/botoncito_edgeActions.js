9803cde64e8f5bc15391d453cc9341c95935f0d1
garrot <Abraham@Abrahams-iMac.local>
2014-07-19 17:38:56 +0200

    Toda la puta web

---
+++ assets/botoncito/botoncito_edgeActions.js
@@ -0,0 +1,20 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",617,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1017,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_botonO}","mouseover",function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Precargador'
+(function(symbolName){})("Precargador");
+//Edge symbol end:'Precargador'
+})(jQuery,AdobeEdge,"EDGE-17626953");
